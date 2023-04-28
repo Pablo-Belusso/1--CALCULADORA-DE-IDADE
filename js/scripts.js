5416783401b4dc6e215d8819eb20b2855ff5f044
Pablo Belusso <pablobelusso@hotmail.com>
2023-04-27 22:13:46 -0300

    Alteração 03

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,59 @@
+// ------    ------
+
+const AnoResultado = document.querySelector('.ano_resultado');
+const MesResultado = document.querySelector('.mes_resultado');
+const DiaResultado = document.querySelector('.dia_resultado');
+const Calcular = document.querySelector('.form-botao');
+
+// ------  Variáreis que receberão os valores dos inputs  ------
+const EntradaDia = document.getElementById('dia'); // Recebe o ano digitado na input
+const EntradaMes = document.getElementById('mes'); // Recebe o ano digitado na input
+const EntradaAno = document.getElementById('ano'); // Recebe o ano digitado na input
+
+
+// ------  CÁLCULO DOS ANOS, MESES E DIAS  ------
+
+function calcularIdade(nascimento) {
+
+    var nascimento = new Date(nascimento) // Variável que irá receber os valores do método 
+    var agora = new Date(); // Variável que irá receber a data atual
+
+    // Cálculo dos ANOS:
+    var anoAtual = agora.getFullYear(); // Método "getFullYear" retorna ano da data especificada.
+    var anoNascimento = nascimento.getFullYear();
+
+    var idadeAno = anoAtual - anoNascimento;
+
+    // Cálculo dos MESES:
+    var idadeMes = agora.getMonth() - nascimento.getMonth(); // Método "getMonth" retorna o mês da data especificada.
 
 
 
-
-calcular.addEventListener('click', function (e) {
-
-    e.preventDefault();
-
-    var dia = document.getElementById('dia').value;
-    var mes = document.getElementById('mes').value;
-    var ano = document.getElementById('ano').value;
-    var calcular = document.getElementById('calcular');
-
-    var agora = new Date();
-    var anoAtual = agora.getFullYear();
-
-    var idadeAno = anoAtual - ano;
-
-    var resultadoFinal = ``;
-
-    resultadoFinal = `Idade é ${idadeAno} anos`;
+    // Cálculo dos DIAS:
+    var idadeDia = agora.getDate() - nascimento.getDate(); // Método "getDate" retorna o dia do mês da data especificada.
 
 
+    // Propriedades que irão escrever no HTML:
+    AnoResultado.innerHTML = idadeAno;
+    MesResultado.innerHTML = idadeMes;
+    DiaResultado.innerHTML = idadeDia;
 
-    console.log(resultadoFinal);
+    console.log(idadeAno);
+    console.log(idadeMes);
+    console.log(idadeDia);
+}
+
+
+// ------  MÉTODO QUE É EXECUTADO APÓS VOCÊ CLICAR NO BOTÃO  ------
+
+Calcular.addEventListener('click', function (e) {
+
+    e.preventDefault(); //  impede que o evento padrão de execução do HTML ocorra
+
+    // Recebe os valores digitados nas inputs
+    let nascimento = `${EntradaMes.value}/${EntradaDia.value}/${EntradaAno.value}`;
+
+    calcularIdade(nascimento); // executa a função
 
 });
 
